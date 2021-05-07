d8f4a14468c70bbece65a7e9f5bbc5e4ba16e3b0
Greenfin17 <jmaple@yahoo.com>
2021-05-06 23:49:06 -0500

    adding user selected team icons

---
+++ src/components/forms/ChooseTheme.js
@@ -0,0 +1,38 @@
+// ChooseTheme.js
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+const ChooseTheme = ({ setTheme, icons }) => {
+  const [ddOpen, setddOpen] = useState(false);
+  const ddToggle = (() => setddOpen(!ddOpen));
+  const handleClick = (e, index) => {
+    e.preventDefault();
+    setTheme(index);
+    console.warn(index);
+  };
+
+  return (
+    <Dropdown className='choose-theme' isOpen={ddOpen} toggle={ddToggle}>
+      <DropdownToggle caret>
+        Theme
+      </DropdownToggle>
+      <DropdownMenu className='team-menu'>
+        {icons.map((team, key) => <DropdownItem
+          key={key} onClick={(e) => handleClick(e, key)}>{team.name} <img src={team.image}/></DropdownItem>)}
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+ChooseTheme.propTypes = {
+  setTheme: PropTypes.func,
+  icons: PropTypes.array
+};
+
+export default ChooseTheme;
---
+++ src/helpers/data/icons.js
@@ -0,0 +1,170 @@
+import sf49ers from '../../assets/images/49ers.gif';
+import bears from '../../assets/images/bears.gif';
+import bengals from '../../assets/images/bengals.gif';
+import bills from '../../assets/images/bills.gif';
+import broncos from '../../assets/images/broncos.gif';
+import browns from '../../assets/images/browns.gif';
+import bucaneers from '../../assets/images/bucaneers.gif';
+import cardinals from '../../assets/images/cardinals.gif';
+import chargers from '../../assets/images/chargers.gif';
+import chiefs from '../../assets/images/chiefs.gif';
+import colts from '../../assets/images/colts.gif';
+import cowboys from '../../assets/images/cowboys.gif';
+import dolphins from '../../assets/images/dolphins.gif';
+import eagles from '../../assets/images/eagles.gif';
+import falcons from '../../assets/images/falcons.gif';
+import giants from '../../assets/images/giants.gif';
+import jaguars from '../../assets/images/jaguars.gif';
+import jets from '../../assets/images/jets.gif';
+import lions from '../../assets/images/lions.gif';
+import packers from '../../assets/images/packers.gif';
+import panthers from '../../assets/images/panthers.gif';
+import patriots from '../../assets/images/patriots.gif';
+import raiders from '../../assets/images/raiders.gif';
+import rams from '../../assets/images/rams.gif';
+import ravens from '../../assets/images/ravens.gif';
+import saints from '../../assets/images/saints.gif';
+import seattle from '../../assets/images/seattle.gif';
+import steelers from '../../assets/images/steelers.gif';
+import tampaBay from '../../assets/images/tampaBay.gif';
+import texans from '../../assets/images/texans.gif';
+import titans from '../../assets/images/titans.gif';
+import vikings from '../../assets/images/vikings.gif';
+import washington from '../../assets/images/washington.gif';
+
+const icons = [
+  {
+    image: sf49ers,
+    name: '49ers'
+  },
+  {
+    image: bears,
+    name: 'Bears'
+  },
+  {
+    image: bengals,
+    name: 'Bengals'
+  },
+  {
+    image: bills,
+    name: 'Bills'
+  },
+  {
+    image: broncos,
+    name: 'Broncos'
+  },
+  {
+    image: browns,
+    name: 'Browns'
+  },
+  {
+    image: bucaneers,
+    name: 'Bucaneers'
+  },
+  {
+    image: cardinals,
+    name: 'Cardinals'
+  },
+  {
+    image: chargers,
+    name: 'Chargers'
+  },
+  {
+    image: chiefs,
+    name: 'Chiefs'
+  },
+  {
+    image: colts,
+    name: 'Colts'
+  },
+  {
+    image: cowboys,
+    name: 'Cowboys'
+  },
+  {
+    image: dolphins,
+    name: 'Dolphins'
+  },
+  {
+    image: eagles,
+    name: 'Eagles'
+  },
+  {
+    image: falcons,
+    name: 'Falcons'
+  },
+  {
+    image: giants,
+    name: 'Giants'
+  },
+  {
+    image: jaguars,
+    name: 'Jaguars'
+  },
+  {
+    image: jets,
+    name: 'Jets'
+  },
+  {
+    image: lions,
+    name: 'Lions'
+  },
+  {
+    image: packers,
+    name: 'Packers'
+  },
+  {
+    image: panthers,
+    name: 'Panthers'
+  },
+  {
+    image: patriots,
+    name: 'Patriots'
+  },
+  {
+    image: raiders,
+    name: 'Raiders'
+  },
+  {
+    image: rams,
+    name: 'Rams'
+  },
+  {
+    image: ravens,
+    name: 'Ravens'
+  },
+  {
+    image: saints,
+    name: 'Saints'
+  },
+  {
+    image: seattle,
+    name: 'Seattle'
+  },
+  {
+    image: steelers,
+    name: 'Steelers'
+  },
+  {
+    image: tampaBay,
+    name: 'TampaBay'
+  },
+  {
+    image: texans,
+    name: 'Texans'
+  },
+  {
+    image: titans,
+    name: 'Titans'
+  },
+  {
+    image: vikings,
+    name: 'Vikings'
+  },
+  {
+    image: washington,
+    name: 'Washington'
+  }
+];
+
+export default icons;
